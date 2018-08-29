1fa5cdac22a7d310405afa6d0fb2875c6815d70b
Ermain <paulermain@gmail.com>
2018-08-28 21:49:15 -0400

    Adding functionality to write the data to a .csv file. Still have problems with the FileNotFoundException.

--- src/main/scala/org/ermain/scala/quandl4j/finance_data/QuandlMain.scala
+++ src/main/scala/org/ermain/scala/quandl4j/finance_data/QuandlMain.scala
@@ -1,10 +1,10 @@
 package org.ermain.scala.quandl4j.finance_data
 
-import java.io.{BufferedWriter, FileWriter}
-
-import com.jimmoores.quandl.{DataSetRequest, QuandlSession}
+import java.io.{File, PrintWriter}
+import scala.io.Source._
+import com.jimmoores.quandl.DataSetRequest
 import com.jimmoores.quandl.tablesaw.TableSawQuandlSession
-import tech.tablesaw.api.{ShortColumn, Table}
+import tech.tablesaw.api.Table
 
 object QuandlMain {
 
@@ -13,16 +13,27 @@ object QuandlMain {
 
     val session: TableSawQuandlSession = TableSawQuandlSession.create()
 
-    val table = session.getDataSet(
+    val table: Table = session.getDataSet(
       DataSetRequest.Builder.of("WIKI/GOOGL")
-        .withMaxRows(20)
+        .withMaxRows(1000)
         .build()
     )
 
-    println(table)
+    // println(table)
+    getQuandlStockData("WIKI/GOOGL")
+
+    val url = "http://archive.ics.uci.edu/ml/machine-learning-databases/00291/airfoil_self_noise.dat"
+    val file_name = "self-noise.csv"
+
+//    val file = new File(file_name)
+//    if (!file.exists()){
+//      val s = new PrintWriter(file_name)
+//      val data = scala.io.Source.fromURL(url).getLines
+//
+//      data.foreach(l =>  s.write(l.trim.split('\t').filter(_ != "").mkString("", ",", "\n")))
+//      s.close()
+//    }
   }
-
-
   def getQuandlStockData(ticker: String, maxRows: Int = 100): Unit = {
 
     try{
@@ -33,7 +44,19 @@ object QuandlMain {
           .of(ticker)
           .withMaxRows(maxRows)
           .build()
-      )
+      ).toString
+
+      val file_name = s"$ticker.csv"
+
+      val file_stock = new File(file_name)
+
+      if (!file_stock.exists()){
+        val s = new PrintWriter(file_name)
+        val data = fromString(table).getLines()
+
+        data.foreach(l =>  s.write(l.trim.split('\t').filter(_ != "").mkString("", ",", "\n")))
+        s.close()
+      }
 
       println(s"Printing data for $ticker")
       println(table)
